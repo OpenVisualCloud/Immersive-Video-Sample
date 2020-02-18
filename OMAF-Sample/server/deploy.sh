b02a86a5514036cefed3631e6575ac139c30ef61
KangXin <kangxin@kx-immersive-desktop.sh.intel.com>
2020-02-18 16:51:18 +0800

    Update build scripts:
    1. Fix errors in server build process.
    2. Modify CMakeLists.txt to solve compilation order issues.
    3. Enable server build process on Ubuntu system.

--- OMAF-Sample/server/deploy.sh
+++ OMAF-Sample/server/deploy.sh
@@ -11,12 +11,12 @@ cd ../..
 cp -r src/360SCVP OMAF-Sample/server/src
 cp -r src/external OMAF-Sample/server/src
 cp -r src/ffmpeg OMAF-Sample/server/src
-cp -r src/OmafDashAccess OMAF-Sample/server/src
 cp -r src/player OMAF-Sample/server/src
 cp -r src/utils OMAF-Sample/server/src
 cp -r src/VROmafPacking OMAF-Sample/server/src
 cp -r src/CMakeLists.txt OMAF-Sample/server/src
 cp -r Sample-Videos OMAF-Sample/server/src
+ls src/OmafDashAccess/ | grep -v mp4lib | xargs -i cp -r src/OmafDashAccess/{} OMAF-Sample/server/src
 cd OMAF-Sample/server
 
 if [ $# = 0 ] ; then
