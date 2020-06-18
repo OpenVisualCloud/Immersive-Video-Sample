ea0d44d3455f662df78f647a5ea3c03baaf5e909
Kang, Xin1 <xin1.kang@intel.com>
2020-06-18 16:29:45 +0800

    Update Dockerfile and demo commands

--- OMAF-Sample/server/run.sh
+++ OMAF-Sample/server/run.sh
@@ -25,19 +25,89 @@ if [ "${TYPE}" != "LIVE" ] && [ "${TYPE}" != "VOD" ] ; then
 fi
 
 ffmpeg_4K_LIVE(){
-    ./ffmpeg -re -stream_loop -1 -i $1 -input_type 1 -rc 1 -c:v:0 distributed_encoder -s:0 3840x1920 -tile_row:0 6 -tile_column:0 10 -config_file:0 config_high.txt  -la_depth:0 0 -r:0 30 -g:0 15 -b:0 30M -map 0:v -c:v:1 distributed_encoder -s:1 1024x640 -tile_row:1 2 -tile_column:1 4 -config_file:1 config_low.txt  -la_depth:1 0 -r:1 30 -g:1 15 -b:1 5M -map 0:v -vframes 10000 -f omaf_packing -is_live 1 -split_tile 1 -seg_duration 1 -window_size 20 -extra_window_size 30 -base_url https://$2:443/LIVE4K/ -out_name Test /usr/local/nginx/html/LIVE4K/
+    ./ffmpeg -stream_loop -1 \
+        -i $1 -input_type 1 -rc 1 \
+        -c:v:0 distributed_encoder \
+        -s:0 3840x1920 \
+        -tile_row:0 6 -tile_column:0 12 \
+        -config_file:0 config_high.txt \
+        -la_depth:0 0 -r:0 30 -g:0 15 \
+        -b:0 30M -map 0:v \
+        -c:v:1 distributed_encoder \
+        -s:1 1024x640 \
+        -tile_row:1 2 -tile_column:1 2 \
+        -config_file:1 config_low.txt \
+        -la_depth:1 0 -r:1 30 -g:1 15 \
+        -b:1 5M -map 0:v \
+        -f omaf_packing \
+        -is_live 1 -split_tile 1 -seg_duration 1 \
+        -window_size 20 -extra_window_size 30 \
+        -base_url https://$2:443/LIVE4K/ \
+        -out_name Test /usr/local/nginx/html/LIVE4K/
 }
 
 ffmpeg_4K_VOD(){
-    ./ffmpeg -stream_loop -1 -i $1 -input_type 1 -rc 1 -r 30 -c:v:0 distributed_encoder -s:0 3840x1920 -g:0 15 -tile_row:0 6 -tile_column:0 10 -la_depth:0 0 -config_file:0 config_high.txt -b:0 30M -map 0:v -c:v:1 distributed_encoder -s:1 1024x640 -sws_flags neighbor -g:1 15 -tile_row:1 1 -tile_column:1 4 -la_depth:1 0 -config_file:1 config_low.txt -b:1 2M -map 0:v -vframes 500 -f omaf_packing -is_live 0 -split_tile 1 -seg_duration 1 -base_url https://$2:443/VOD4K/ -out_name Test /usr/local/nginx/html/VOD4K/
+    ./ffmpeg -stream_loop -1 \
+        -i $1 -input_type 1 -rc 1 \
+        -c:v:0 distributed_encoder \
+        -s:0 3840x1920 \
+        -tile_row:0 6 -tile_column:0 12 \
+        -config_file:0 config_high.txt \
+        -la_depth:0 0 -r:0 30 -g:0 15 \
+        -b:0 30M -map 0:v \
+        -c:v:1 distributed_encoder \
+        -s:1 1024x640 -sws_flags neighbor \
+        -tile_row:1 2 -tile_column:1 2 \
+        -config_file:1 config_low.txt \
+        -la_depth:1 0 -r:1 30 -g:1 15 \
+        -b:1 2M -map 0:v -vframes 500 \
+        -f omaf_packing \
+        -is_live 0 -split_tile 1 -seg_duration 1 \
+        -base_url https://$2:443/VOD4K/ \
+        -out_name Test /usr/local/nginx/html/VOD4K/
 }
 
 ffmpeg_8K_LIVE(){
-    numactl -c 1 ./ffmpeg -stream_loop -1 -re -i $1 -input_type 1 -c:v:0 distributed_encoder -s:0 7680x3840 -g:0 25 -tile_row:0 6 -tile_column:0 12 -la_depth:0 0 -config_file:0 config_high.txt -b:0 50M -map 0:v -c:v:1 distributed_encoder -s:1 1280x1280 -sws_flags neighbor -g:1 25 -tile_row:1 2 -tile_column:1 2 -la_depth:1 0 -config_file:1 config_low.txt -b:1 2M -map 0:v -f omaf_packing -is_live 1 -split_tile 1 -seg_duration 1 -extractors_per_thread 4 -base_url https://$2:443/LIVE8K/ -out_name Test /usr/local/nginx/html/LIVE8K/
+    numactl -c 1 ./ffmpeg -stream_loop -1 \
+        -i $1 -input_type 1 -rc 1 \
+        -c:v:0 distributed_encoder \
+        -s:0 7680x3840 \
+        -tile_row:0 6 -tile_column:0 12 \
+        -config_file:0 config_high.txt \
+        -la_depth:0 0 -r:0 25 -g:0 25 \
+        -b:0 50M -map 0:v \
+        -c:v:1 distributed_encoder \
+        -s:1 1280x1280 -sws_flags neighbor \
+        -tile_row:1 2 -tile_column:1 2 \
+        -config_file:1 config_low.txt \
+        -la_depth:1 0 -r:1 25 -g:1 25 \
+        -b:1 2M -map 0:v \
+        -f omaf_packing \
+        -is_live 1 -split_tile 1 -seg_duration 1 \
+        -extractors_per_thread 4 \
+        -base_url https://$2:443/LIVE8K/ \
+        -out_name Test /usr/local/nginx/html/LIVE8K/
 }
 
 ffmpeg_8K_VOD(){
-    numactl -c 1 ./ffmpeg -stream_loop -1 -i $1 -input_type 1 -rc 1 -r 25 -c:v:0 distributed_encoder -s:0 7680x3840 -g:0 25 -tile_row:0 6 -tile_column:0 12 -la_depth:0 0 -config_file:0 config_high.txt -b:0 50M -map 0:v -c:v:1 distributed_encoder -s:1 1280x1280 -sws_flags neighbor -g:1 25 -tile_row:1 2 -tile_column:1 2 -la_depth:1 0 -config_file:1 config_low.txt -b:1 2M -map 0:v -vframes 500 -f omaf_packing -is_live 0 -split_tile 1 -seg_duration 1 -base_url https://$2:443/VOD8K/ -out_name Test /usr/local/nginx/html/VOD8K/
+    numactl -c 1 ./ffmpeg -stream_loop -1 \
+        -i $1 -input_type 1 -rc 1 \
+        -c:v:0 distributed_encoder \
+        -s:0 7680x3840 \
+        -tile_row:0 6 -tile_column:0 12 \
+        -config_file:0 config_high.txt \
+        -la_depth:0 0 -r:0 25 -g:0 25 \
+        -b:0 50M -map 0:v \
+        -c:v:1 distributed_encoder \
+        -s:1 1280x1280 -sws_flags neighbor \
+        -tile_row:1 2 -tile_column:1 2 \
+        -config_file:1 config_low.txt \
+        -la_depth:1 0 -r:1 25 -g:1 25 \
+        -b:1 2M -map 0:v -vframes 500 \
+        -f omaf_packing \
+        -is_live 0 -split_tile 1 -seg_duration 1 \
+        -base_url https://$2:443/VOD8K/ \
+        -out_name Test /usr/local/nginx/html/VOD8K/
 }
 
 export LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/lib64:$LD_LIBRARY_PATH
