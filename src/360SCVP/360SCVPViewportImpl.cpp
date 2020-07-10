e56bda42e9e24258acca895c6bb9fd040dba6e2f
Chen Xiaomin <xiaomin.chen@intel.com>
2020-07-10 14:12:05 +0800

    Modify face index for selected tiles when source is from cubemap projection
    Signed-off-by: Chen Xiaomin <xiaomin.chen@intel.com>

--- src/360SCVP/360SCVPViewportImpl.cpp
+++ src/360SCVP/360SCVPViewportImpl.cpp
@@ -48,6 +48,7 @@ using namespace std;
 #define HIGH_PITCH_BOUND_IN_NORTH 50
 #define HIGH_PITCH_BOUND_IN_SOUTH -50
 
+int32_t cubeMapFaceMap[6] = {0, 1, 4, 5, 2, 3};
 void* genViewport_Init(generateViewPortParam* pParamGenViewport)
 {
     uint32_t maxTileNumCol = 0;
@@ -462,116 +463,124 @@ int32_t genViewport_getTilesInViewport(void* pGenHandle, TileDef* pOutTile)
 
     int32_t fPitch = (int32_t)(cTAppConvCfg->m_codingSVideoInfo.viewPort.fPitch);
     tileNum = cTAppConvCfg->calcTilesInViewport(cTAppConvCfg->m_srd, cTAppConvCfg->m_tileNumCol, cTAppConvCfg->m_tileNumRow);
-
-    int32_t pos = 0;
-    if (fPitch >= LOW_PITCH_BOUND_IN_NORTH || fPitch <= LOW_PITCH_BOUND_IN_SOUTH)
+    if (cTAppConvCfg->m_sourceSVideoInfo.geoType == SVIDEO_EQUIRECT)
     {
-        if (fPitch >= LOW_PITCH_BOUND_IN_NORTH)
-            pos = 0;
-        else
-            pos = cTAppConvCfg->m_tileNumRow - 1;
+        int32_t pos = 0;
+        if (fPitch >= LOW_PITCH_BOUND_IN_NORTH || fPitch <= LOW_PITCH_BOUND_IN_SOUTH)
+        {
+            if (fPitch >= LOW_PITCH_BOUND_IN_NORTH)
+                pos = 0;
+            else
+                pos = cTAppConvCfg->m_tileNumRow - 1;
 
-        int32_t leftAddition = additionalTilesNum / 2;
-        int32_t rightAddition = additionalTilesNum - leftAddition;
-        for (uint32_t j = pos * cTAppConvCfg->m_tileNumCol; j < (pos+1) * cTAppConvCfg->m_tileNumCol; j++)
-        {
-            if (cTAppConvCfg->m_srd[j].isOccupy == 0 && cTAppConvCfg->m_srd[(j+1)%cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol].isOccupy == 1)
+            int32_t leftAddition = additionalTilesNum / 2;
+            int32_t rightAddition = additionalTilesNum - leftAddition;
+            for (uint32_t j = pos * cTAppConvCfg->m_tileNumCol; j < (pos+1) * cTAppConvCfg->m_tileNumCol; j++)
             {
-                uint32_t acc_idx = (j + cTAppConvCfg->m_tileNumCol) % cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol;
-                while (leftAddition-- > 0)
+                if (cTAppConvCfg->m_srd[j].isOccupy == 0 && cTAppConvCfg->m_srd[(j+1)%cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol].isOccupy == 1)
                 {
-                    cTAppConvCfg->m_srd[acc_idx].isOccupy = 1;
-                    if (pos == 0)
+                    uint32_t acc_idx = (j + cTAppConvCfg->m_tileNumCol) % cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol;
+                    while (leftAddition-- > 0)
                     {
-                        acc_idx += cTAppConvCfg->m_tileNumCol;
-                    }else
+                        cTAppConvCfg->m_srd[acc_idx].isOccupy = 1;
+                        if (pos == 0)
+                        {
+                            acc_idx += cTAppConvCfg->m_tileNumCol;
+                        }else
+                        {
+                            acc_idx -= cTAppConvCfg->m_tileNumCol;
+                        }
+                    }
+                }
+                if (cTAppConvCfg->m_srd[j].isOccupy == 1 && cTAppConvCfg->m_srd[(j+1)%cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol].isOccupy == 0)
+                {
+                    uint32_t acc_idx = (j + 1 + cTAppConvCfg->m_tileNumCol) % cTAppConvCfg->m_tileNumCol +  pos * cTAppConvCfg->m_tileNumCol;
+                    while (rightAddition-- > 0)
                     {
-                        acc_idx -= cTAppConvCfg->m_tileNumCol;
+                        cTAppConvCfg->m_srd[acc_idx].isOccupy = 1;
+                        if (pos == 0)
+                        {
+                            acc_idx += cTAppConvCfg->m_tileNumCol;
+                        }else
+                        {
+                            acc_idx -= cTAppConvCfg->m_tileNumCol;
+                        }
                     }
                 }
             }
-            if (cTAppConvCfg->m_srd[j].isOccupy == 1 && cTAppConvCfg->m_srd[(j+1)%cTAppConvCfg->m_tileNumCol + pos * cTAppConvCfg->m_tileNumCol].isOccupy == 0)
+            // work around : high pitch area need more tiles( 2 rows )
+            if (fPitch >= HIGH_PITCH_BOUND_IN_NORTH)
             {
-                uint32_t acc_idx = (j + 1 + cTAppConvCfg->m_tileNumCol) % cTAppConvCfg->m_tileNumCol +  pos * cTAppConvCfg->m_tileNumCol;
-                while (rightAddition-- > 0)
+                for (uint32_t i = 0; i < min(cTAppConvCfg->m_tileNumCol * 2, cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow); i++)
                 {
-                    cTAppConvCfg->m_srd[acc_idx].isOccupy = 1;
-                    if (pos == 0)
-                    {
-                        acc_idx += cTAppConvCfg->m_tileNumCol;
-                    }else
-                    {
-                        acc_idx -= cTAppConvCfg->m_tileNumCol;
-                    }
+                    if (cTAppConvCfg->m_srd[i].isOccupy == 0)
+                        additionalTilesNum++;
+                    cTAppConvCfg->m_srd[i].isOccupy = 1;
                 }
             }
-        }
-        // work around : high pitch area need more tiles( 2 rows )
-        if (fPitch >= HIGH_PITCH_BOUND_IN_NORTH)
-        {
-            for (uint32_t i = 0; i < min(cTAppConvCfg->m_tileNumCol * 2, cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow); i++)
+            if (fPitch <= HIGH_PITCH_BOUND_IN_SOUTH)
             {
-                if (cTAppConvCfg->m_srd[i].isOccupy == 0)
-                    additionalTilesNum++;
-                cTAppConvCfg->m_srd[i].isOccupy = 1;
-            }
-        }
-        if (fPitch <= HIGH_PITCH_BOUND_IN_SOUTH)
-        {
-            uint32_t startRow = cTAppConvCfg->m_tileNumRow - 2 > 0 ? cTAppConvCfg->m_tileNumRow - 2 : 0;
-            for (uint32_t i = startRow * cTAppConvCfg->m_tileNumCol; i < cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow; i++)
-            {
-                if (cTAppConvCfg->m_srd[i].isOccupy == 0)
-                    additionalTilesNum++;
-                cTAppConvCfg->m_srd[i].isOccupy = 1;
+                uint32_t startRow = cTAppConvCfg->m_tileNumRow - 2 > 0 ? cTAppConvCfg->m_tileNumRow - 2 : 0;
+                for (uint32_t i = startRow * cTAppConvCfg->m_tileNumCol; i < cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow; i++)
+                {
+                    if (cTAppConvCfg->m_srd[i].isOccupy == 0)
+                        additionalTilesNum++;
+                    cTAppConvCfg->m_srd[i].isOccupy = 1;
+                }
             }
         }
     }
     //set the occupy tile into the output parameter
     int32_t idx = 0;
     TileDef* pOutTileTmp = pOutTile;
-    // correct accurate needed tile number.
-    if (additionalTilesNum > 0)
+    int32_t faceNum = (cTAppConvCfg->m_sourceSVideoInfo.geoType == SVIDEO_CUBEMAP) ? 6 : 2;
+    // correct accurate needed tile number in ERP.
+    if (additionalTilesNum > 0 && faceNum == 2)
         tileNum = tileNum + additionalTilesNum;
 
     uint32_t occupancyNum = 0;
     if (cTAppConvCfg->m_srd[cTAppConvCfg->m_tileNumCol*cTAppConvCfg->m_tileNumRow-1].isOccupy == 1)
     {
-        idx = cTAppConvCfg->m_tileNumCol*cTAppConvCfg->m_tileNumRow -1;
-        for (uint32_t col = cTAppConvCfg->m_tileNumCol; col > 0 ; col--)
+        for (int32_t idFace = 0; idFace < faceNum; idFace++)
         {
-            for (uint32_t row = cTAppConvCfg->m_tileNumRow; row > 0 ; row--)
+            idx = idFace * cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow + cTAppConvCfg->m_tileNumCol * cTAppConvCfg->m_tileNumRow - 1;
+            for (uint32_t col = cTAppConvCfg->m_tileNumCol; col > 0 ; col--)
             {
-                if (cTAppConvCfg->m_srd[idx].isOccupy == 1)
+                for (uint32_t row = cTAppConvCfg->m_tileNumRow; row > 0 ; row--)
                 {
-                    pOutTileTmp->faceId = cTAppConvCfg->m_srd[idx].faceId;
-                    pOutTileTmp->x = cTAppConvCfg->m_srd[idx].x;
-                    pOutTileTmp->y = cTAppConvCfg->m_srd[idx].y;
-                    pOutTileTmp->idx = idx;
-                    pOutTileTmp++;
-                    occupancyNum++;
+                    if (cTAppConvCfg->m_srd[idx].isOccupy == 1)
+                    {
+                        pOutTileTmp->faceId = cubeMapFaceMap[cTAppConvCfg->m_srd[idx].faceId];
+                        pOutTileTmp->x = cTAppConvCfg->m_srd[idx].x;
+                        pOutTileTmp->y = cTAppConvCfg->m_srd[idx].y;
+                        pOutTileTmp->idx = idx;
+                        pOutTileTmp++;
+                        occupancyNum++;
+                    }
+                    idx--;
                 }
-                idx--;
             }
         }
-
     }
     else
     {
-        for (uint32_t col = 0; col < cTAppConvCfg->m_tileNumCol; col++)
+        for (int32_t idFace = 0; idFace < faceNum; idFace++)
         {
-            for (uint32_t row = 0; row < cTAppConvCfg->m_tileNumRow; row++)
+            for (uint32_t col = 0; col < cTAppConvCfg->m_tileNumCol; col++)
             {
-                if (cTAppConvCfg->m_srd[idx].isOccupy == 1)
+                for (uint32_t row = 0; row < cTAppConvCfg->m_tileNumRow; row++)
                 {
-                    pOutTileTmp->faceId = cTAppConvCfg->m_srd[idx].faceId;
-                    pOutTileTmp->x = cTAppConvCfg->m_srd[idx].x;
-                    pOutTileTmp->y = cTAppConvCfg->m_srd[idx].y;
-                    pOutTileTmp->idx = idx;
-                    pOutTileTmp++;
-                    occupancyNum++;
+                    if (cTAppConvCfg->m_srd[idx].isOccupy == 1)
+                    {
+                        pOutTileTmp->faceId = cubeMapFaceMap[cTAppConvCfg->m_srd[idx].faceId];
+                        pOutTileTmp->x = cTAppConvCfg->m_srd[idx].x;
+                        pOutTileTmp->y = cTAppConvCfg->m_srd[idx].y;
+                        pOutTileTmp->idx = idx;
+                        pOutTileTmp++;
+                        occupancyNum++;
+                    }
+                    idx++;
                 }
-                idx++;
             }
         }
     }
@@ -1141,7 +1150,7 @@ int32_t TgenViewport::calcTilesInViewport(ITileInfo* pTileInfo, int32_t tileCol,
                 if (pTileInfoTmp->isOccupy == 1)
                 {
                     ret++;
-                    printf("facid, x, y : %d, %d, %d\n", faceid, pTileInfoTmp->x, pTileInfoTmp->y);
+                    printf("facid, x, y : %d, %d, %d\n", cubeMapFaceMap[faceid], pTileInfoTmp->x, pTileInfoTmp->y);
                 }
                 pTileInfoTmp++;
             }
