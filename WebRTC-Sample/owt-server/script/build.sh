0ac9d30d59ae187efb5c00d6953bcbbc6dda1141
Jianhui Dai <jianhui.j.dai@intel.com>
2020-12-14 19:58:21 +0800

    Fix sudo

--- WebRTC-Sample/owt-server/script/build.sh
+++ WebRTC-Sample/owt-server/script/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
-if test -z "${DIR}"; then 
+SUDO=""
+if [[ $EUID -ne 0 ]]; then
+  SUDO="sudo -E"
+fi
+
+if test -z "${DIR}"; then
     echo "This script should not be called directly."
     exit -1
-fi 
+fi
 
 USER="docker"
 GROUP="docker"
@@ -15,9 +20,9 @@ for dep in '.8.*' '.7.*' '.6.*' '.5.*' '.4.*' '.3.*' '.2.*' '.1.*' ''; do
         if test -z "$image"; then image="$IMAGE"; fi
 
         if grep -q 'AS build' "$dockerfile"; then
-            sudo docker build --network=host --file="$dockerfile" --target build -t "$image:build" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+            ${SUDO} docker build --network=host --file="$dockerfile" --target build -t "$image:build" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
         fi
 
-        sudo docker build --network=host --file="$dockerfile" -t "$image:latest" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
+        ${SUDO} docker build --network=host --file="$dockerfile" -t "$image:latest" "$DIR" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') --build-arg USER=${USER} --build-arg GROUP=${GROUP} --build-arg UID=$(id -u) --build-arg GID=$(id -g)
     done
 done
