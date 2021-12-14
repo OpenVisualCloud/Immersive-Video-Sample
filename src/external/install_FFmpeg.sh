6a309faab9239ef9e29df5f0e685d70004137330
U1X6WK <xin1.kang@intel.com>
2021-12-14 09:09:45 +0800

    Update build scripts and documentation typos

--- src/external/install_FFmpeg.sh
+++ src/external/install_FFmpeg.sh
@@ -1,18 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 TARGET=$1
 REPO=$2
+FFMPEG_REPO=FFmpeg
 
 cd ..
 if [ "${REPO}" != "oss" ] ; then
-    if [ ! -d "./FFmpeg" ] ; then
-        if [ ! -f "./ffmpeg-4.3.1.tar.xz" ] ; then
-            wget http://ffmpeg.org/releases/ffmpeg-4.3.1.tar.xz
-        fi
-        tar xf ffmpeg-4.3.1.tar.xz && mv ffmpeg-4.3.1 FFmpeg
+    if [ ! -d "${FFMPEG_REPO}" ] ; then
+        mkdir ${FFMPEG_REPO} && cd ${FFMPEG_REPO}
+        git init
+        git remote add origin https://github.com/FFmpeg/FFmpeg.git
+        git fetch --depth 1 origin 6b6b9e593dd4d3aaf75f48d40a13ef03bdef9fdb
+        git checkout FETCH_HEAD
+        cd -
     fi
-    if [ ! -f "FFmpeg/libavcodec/distributed_encoder.c" ] ; then
-        cd FFmpeg
+    if [ ! -f "${FFMPEG_REPO}/libavcodec/distributed_encoder.c" ] ; then
+        cd ${FFMPEG_REPO}
         patch -p1 < ../ffmpeg/patches/FFmpeg_OMAF.patch
         cd ..
     fi
@@ -22,9 +25,9 @@ if [ "${TARGET}" == "server" ] ; then
 
     mkdir -p build/external/ffmpeg_server
     cd build/external/ffmpeg_server
-    ../../../FFmpeg/configure --prefix=/usr --libdir=/usr/local/lib \
+    ../../../${FFMPEG_REPO}/configure --prefix=/usr --libdir=/usr/local/lib \
         --enable-static --enable-shared --enable-gpl --enable-nonfree \
-        --disable-optimizations --disable-vaapi --disable-filter=erp2cubmap_mdf
+        --disable-optimizations --disable-vaapi
     make -j $(nproc)
     sudo make install
 
@@ -32,7 +35,7 @@ elif [ "${TARGET}" == "client" ] ; then
 
     mkdir -p build/external/ffmpeg_client
     cd build/external/ffmpeg_client
-    ../../../FFmpeg/configure --enable-shared --disable-filter=erp2cubmap_mdf
+    ../../../${FFMPEG_REPO}/configure --enable-shared
     make -j $(nproc)
     sudo make install
 
