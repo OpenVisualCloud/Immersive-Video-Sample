4fd3ec8d39214da9baad22a74090c409a6b2629d
hzhan80 <andrew.zhang@intel.com>
2025-07-22 20:31:39 +0800

    Revert changes to ./src/external/run_test.sh from commit 426167b

--- src/external/run_test.sh
+++ src/external/run_test.sh
@@ -45,15 +45,18 @@ if [ "${REPO}" = "oss" ] ; then
     cd OmafDashAccess
     curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
 
-    # ./testMediaSource --gtest_filter=*_static
-    # ./testMediaSource --gtest_filter=*_live
-    # ./testMediaSource --gtest_filter=*_static_withPredictor
-    # ./testMediaSource --gtest_filter=*_live_withPredictor
-    # ./testMediaSource --gtest_filter=*_static_changeViewport
-    # ./testMediaSource --gtest_filter=*_live_changeViewport
+    ./testMediaSource --gtest_filter=*_static
+    ./testMediaSource --gtest_filter=*_live
+    ./testMediaSource --gtest_filter=*_static_withPredictor
+    ./testMediaSource --gtest_filter=*_live_withPredictor
+    ./testMediaSource --gtest_filter=*_static_changeViewport
+    ./testMediaSource --gtest_filter=*_live_changeViewport
     # local dash file will be deleted after release.
-    # ./testMPDParser
+    curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
+    ./testMPDParser
+    curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
     ./testOmafReader
+    curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
     ./testOmafReaderManager
 
     rm -rf ./segs_for_readertest*
