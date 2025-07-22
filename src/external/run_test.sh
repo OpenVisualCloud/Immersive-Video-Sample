a239a8803479e4dec8c589636a51542bda7f2638
hzhan80 <andrew.zhang@intel.com>
2025-07-22 20:31:39 +0800

    Revert changes to ./src/external/run_test.sh from commit 1ca1d1d

--- src/external/run_test.sh
+++ src/external/run_test.sh
@@ -43,8 +43,8 @@ if [ "${REPO}" = "oss" ] ; then
     # OmafDashAccess test
     ################################
     cd OmafDashAccess
-#    curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
-    curl -ukang1:APo9wQK1WNV6vp3bD4rNtRLohp -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
+    curl -H 'X-JFrog-Art-Api: AKCp5dL3Kxmp2PhDfYhT2oFk4SDxJji5H8S38oAqmMSkiD46Ho8uCA282aJJhM9ZqCKLb64bw' -O "https://ubit-artifactory-sh.intel.com/artifactory/immersive_media-sh-local/testfile/segs_for_readertest_0909.tar.gz" && tar zxf segs_for_readertest_0909.tar.gz
+
     # ./testMediaSource --gtest_filter=*_static
     # ./testMediaSource --gtest_filter=*_live
     # ./testMediaSource --gtest_filter=*_static_withPredictor
